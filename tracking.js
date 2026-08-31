bc5fd94953f354a3665e4b53b8fa97573d224b9d
claudeaicapecoral <claudeaicapecoral@gmail.com>
2026-08-31 13:16:17 -0700

    Add Google Ads tag AW-18033973772 next to Meta pixel

--- tracking.js
+++ tracking.js
@@ -1,3 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18033973772');
+(function(){var s=document.createElement('script');s.async=true;s.src='https://www.googletagmanager.com/gtag/js?id=AW-18033973772';document.head.appendChild(s);})();
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '341702038742463');
 fbq('track', 'PageView');
